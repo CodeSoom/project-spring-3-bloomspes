400a858f02c016c1a9f6d9d4b30e70ce1137dca3
Spes <bloomspes@gmail.com>
2021-10-18 19:05:20 +0900

    init commit

---
+++ src/test/java/com/codesoom/project/ResilientApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codesoom.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ResilientApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
